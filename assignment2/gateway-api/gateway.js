e2c73ef24112f3c440932df41031f27bafea9b17
Kumar Satyam <kr.satyam.indgmail.com>
2017-09-15 14:51:16 -0400

    dcokerized gateway and comminication between gateway and python service is happening throgh RMQ

--- assignment2/gateway-api/gateway.js
+++ assignment2/gateway-api/gateway.js
@@ -1,12 +1,20 @@
 var express = require('express');
-
+var amqp = require('amqplib/callback_api');
 var app = express();
 
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.get('/helloworld', function(req,res){
 	res.send("hello World");
 });
+
+
+
 
 //onboarding an employee service
 app.post('/addEmployeeGateway', function(req,res){
@@ -15,12 +23,110 @@ app.post('/addEmployeeGateway', function(req,res){
 
 //department-salary service
 app.post('/addSalaryGateway', function(req,res){
-  res.redirect(307,'http://localhost:5002/addSalarySlab');
+    
+    const designation = req.body.designation;
+    const dept = req.body.dept;
+    const pay = req.body.pay;
+   
+    console.log(designation);
+    console.log(dept);
+    console.log(pay);
+  
+
+    var payload = require('amqplib/callback_api');
+    
+    amqp.connect('amqp://172.17.0.3', function(err, conn) {
+      conn.createChannel(function(err, ch) {
+        ch.assertQueue('', {exclusive: true}, function(err, q) {
+          var corr = generateUuid();
+  
+          //var reqObj = { 'designation': designation, 'dept': dept, 'pay': pay }
+         // var payload = JSON.stringify(reqObj)
+
+         var payload=designation+","+dept+","+pay;
+    
+          console.log(' [x] Requesting the queue for adding an a salaryslab(%s)', payload);
+          console.log(corr);
+          ch.consume(q.queue, function(msg) {
+            if (msg.properties.correlationId == corr) {
+              console.log(' [.] Got %s', msg.content.toString());
+              setTimeout(function() { conn.close(); process.exit(0) }, 500);
+              res.send("salary slab added successfully");
+            }
+          }, {noAck: true});
+    
+          ch.sendToQueue('create-salary-queue',
+          new Buffer(payload.toString()),
+          { correlationId: corr, replyTo: q.queue });
+        });
+      });
+    });
+    
+    function generateUuid() {
+      return Math.random().toString() +
+             Math.random().toString() +
+             Math.random().toString();
+    }
 });
+
 //fetch salary microservice
 app.get('/findSalary', function(req,res){
 	res.redirect(307,'http://localhost:9090/employee' + req.url);
 });
+
+
+
+
+
+
+/*
+//onboarding an employee service
+app.post('/addEmployeeGateway', function(req,res){
+  
+  const name = req.body.name;
+  const gender = req.body.gender;
+  const dept = req.body.dept;
+  const designation = req.body.designation;
+
+  console.log(name);
+  console.log(gender);
+  console.log(dept);
+  console.log(designation);
+  var payload = require('amqplib/callback_api');
+  
+  amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      ch.assertQueue('', {exclusive: true}, function(err, q) {
+        var corr = generateUuid();
+
+       var payload=name+","+gender+","+dept+","+designation;
+  
+        console.log(' [x] Requesting the queue for adding an employee(%s)', payload);
+        console.log(corr);
+        ch.consume(q.queue, function(msg) {
+          if (msg.properties.correlationId == corr) {
+            console.log(' [.] Got %s', msg.content.toString());
+            setTimeout(function() { conn.close(); process.exit(0) }, 500);
+          }
+        }, {noAck: true});
+  
+        ch.sendToQueue('employee-creation-queue',
+        new Buffer(payload.toString()),
+        { correlationId: corr, replyTo: q.queue });
+      });
+    });
+  });
+  
+  function generateUuid() {
+    return Math.random().toString() +
+           Math.random().toString() +
+           Math.random().toString();
+  }
+  
+});
+
+*/
+
 
 app.get('*', function (req, res) {
   console.log("Bad request");
