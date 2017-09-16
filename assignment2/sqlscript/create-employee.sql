a2a81bf65f31cbfda34e21222dee82e064c5d3c7
Kumar Satyam <kr.satyam.ind@gmail.com>
2017-09-16 03:05:39 -0400

    Update create-employee.sql

--- assignment2/sqlscript/create-employee.sql
+++ assignment2/sqlscript/create-employee.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS employeedb;
-CREATE USER IF NOT EXISTS 'employeeuser'@'localhost' identified by 'ThePassword';
-grant all on employeedb.* to 'employeeuser'@'localhost';
+CREATE USER IF NOT EXISTS 'employeeuser'@'%' identified by 'ThePassword';
+grant all on employeedb.* to 'employeeuser'@'%';
 use employeedb;
 CREATE TABLE IF NOT EXISTS `employee` (`id` int(11) NOT NULL AUTO_INCREMENT ,`name` varchar(255) NOT NULL,`dept` varchar(255) NOT NULL, `designation` varchar(255) NOT NULL,`gender` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
