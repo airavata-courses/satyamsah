c863184a7113f943a18ced48f9200a01cb432452
Kumar Satyam <kr.satyam.ind@gmail.com>
2017-09-20 13:27:21 -0400

    Update build.sh

--- assignment2/build.sh
+++ assignment2/build.sh
@@ -7,10 +7,17 @@ echo IN::$PWD;
 sudo docker rm -f $(sudo docker ps -a -q) || echo No Docker images
 # Delete all images
 sudo docker rmi -f $(sudo docker images -q) || echo No Docker containers
+sleep 5
 sudo docker run -d --name webui-container --hostname webui-container -p 8090:8090 satyamsah/web-interface-image
+sleep 5
 sudo docker run -d --name mysql-container --hostname mysql-container -e MYSQL_ROOT_PASSWORD=root123 satyamsah/mysql
+sleep 5
 sudo docker run -d --name rmq-container --hostname rmq-container --name rmq-container rabbitmq:3
+sleep 5
 sudo docker run -d --name create-emp-container --hostname create-emp-container -p 9090:9090 --link rmq-container --link mysql-container satyamsah/employee-onboard-image
+sleep 5
 sudo docker run -d --name create-salaryslab-container --hostname create-salaryslab-container --link mysql-container satyamsah/create-salaryslab-image
+sleep 5
 sudo docker run -d --name api-gateway-container --hostname api-gateway-container -p 9999:9999 satyamsah/gateway-image
+sleep 5
 sudo docker run -d --name fetch-salary-container --hostname fetch-salary-container -p 3000:3000 --link rmq-container --link mysql-container satyamsah/fetch-salary-image
