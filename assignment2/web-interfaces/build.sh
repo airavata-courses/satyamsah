0847d31dbc33ed2c124cf33957312e6e2813a984
Kumar Satyam <kr.satyam.indgmail.com>
2017-09-25 15:36:46 -0400

    identifeied mistakes in shell script

--- assignment2/web-interfaces/build.sh
+++ assignment2/web-interfaces/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ip=curl -i -H "Accept: application/json" -H "Content-Type: application/json" https://ipv4.icanhazip.com/ | tail -n 1
+ip=`curl -i -H "Accept: application/json" -H "Content-Type: application/json" https://ipv4.icanhazip.com/ | tail -n 1`
 
 echo $ip
 
 sed -i -e "s/localhost/$ip/g" src/main/resources/static/addemployee.html
 sed -i -e "s/localhost/$ip/g" src/main/resources/static/addSalarySlab.html
-sed -i -e "s/localhost/$ip/g" src/main/resources/static/addSalarySlab.html
-sed -i -e "s/localhost/$ip/g" src/main/resources/static/Welcome.html
+sed -i -e "s/localhost/$ip/g" src/main/resources/static/FindSalary.html
+
